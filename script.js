500b3ff008e1a513f77d1bf20d01fe89f3069091
saravananbeta <saravanan.beta@gmail.com>
2026-09-08 10:23:36 +0530

    Update script.js
    Arrow keys added

--- script.js
+++ script.js
@@ -451,4 +451,19 @@ document.addEventListener('keydown', (e) => {
     document.getElementById('playBtn')?.click(); // same as clicking play button
   }
 });
+// Arraw keys to Pre/Next
+document.addEventListener('keydown', (e) => {
+  if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') return;
+
+  if (e.code === 'Space' || e.key === ' ') {
+    e.preventDefault();
+    document.getElementById('playBtn')?.click();
+  } else if (e.code === 'ArrowRight') {
+    e.preventDefault();
+    document.getElementById('nextBtn')?.click();
+  } else if (e.code === 'ArrowLeft') {
+    e.preventDefault();
+    document.getElementById('prevBtn')?.click();
+  }
+});
 
