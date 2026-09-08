505de0f9126eaad0ae9ff1c288a8a9b8bdc91a1a
saravananbeta <saravanan.beta@gmail.com>
2026-09-08 09:59:34 +0530

    Update script.js
    SPACE BAR toggle play

--- script.js
+++ script.js
@@ -441,4 +441,14 @@ function onPlayerStateChange(event) {
         //document.getElementById('trackTitle').innerText = showMovie ? 'Movie' : 'Song';
     }, 5000);
 } 
+// Pause/play toggle by pressing SPACE BAR
+document.addEventListener('keydown', (e) => {
+  // don't trigger if typing in input / textarea
+  if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') return;
+
+  if (e.code === 'Space' || e.key === ' ') {
+    e.preventDefault(); // stop page scroll
+    document.getElementById('playBtn')?.click(); // same as clicking play button
+  }
+});
 
